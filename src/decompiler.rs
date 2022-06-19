af520383d2cbda4f425d5807ac34476e3e7abd68
tdumortier <thibault.dumortier@etu.unistra.fr>
2022-06-19 14:40:02 +0200

    fixed decompiler and added rectangle decomp

--- src/decompiler.rs
+++ src/decompiler.rs
@@ -8,6 +8,13 @@ pub trait Decompiler {
     fn decompile(&self, dir: &Path) -> Result<(), Box<dyn Error>>;
 }
 
+/*
+ * Decompilers for the image type `image::Rgba8Image`
+ * The following decompilers are small simple decompilers
+ * Anyone can use them to decompile an image
+*/
+
+///Decompiles an image through squares of a given dimension
 pub struct SquareDecompiler {
     dim: u32,
 }
@@ -31,9 +38,9 @@ impl Decompiler for SquareDecompiler {
 
             for x in 0..new_img.height() {
                 for y in 0..new_img.width() {
-                    let n = dbg!(y + counter * self.dim);
+                    let n = y + counter * self.dim;
                     let pixel = img.get_pixel(n, x);
-                    new_img.put_pixel(x, y, pixel);
+                    new_img.put_pixel(y, x, pixel);
                 }
             }
             if !Path::new("./decompiledImages").exists() {
@@ -44,6 +51,55 @@ impl Decompiler for SquareDecompiler {
 
             size -= self.dim;
             counter += 1;
+        }
+
+        Ok(())
+    }
+}
+
+///Decompiles an image through rectangles of given dimensions
+pub struct RectangleDecompiler {
+    width: u32,
+    height: u32,
+}
+impl RectangleDecompiler {
+    pub fn new(width: u32, height: u32) -> RectangleDecompiler {
+        RectangleDecompiler { width, height }
+    }
+}
+impl Decompiler for RectangleDecompiler {
+    fn decompile(&self, img: &Path) -> Result<(), Box<dyn Error>> {
+        assert!(check_if_image(img));
+
+        println!("now decompiling: {}", img.display());
+
+        let img = open(img).unwrap();
+        let mut size = img.width();
+        let mut counter = 0;
+
+        if img.height() >= self.height {
+            while size > 0 {
+                let mut new_img = DynamicImage::new_rgba8(self.width, self.height);
+
+                for x in 0..new_img.height() {
+                    for y in 0..new_img.width() {
+                        let n = y + counter * self.width;
+                        let pixel = img.get_pixel(n, x);
+                        new_img.put_pixel(y, x, pixel);
+                    }
+                }
+                if !Path::new("./decompiledImages").exists() {
+                    create_dir(Path::new("./decompiledImages"))?;
+                }
+
+                image_writer(new_img, format!("./decompiledImages/{}.png", counter))?;
+
+                size -= self.width;
+                counter += 1;
+                print!("{}", counter);
+            }
+        } else {
+            eprintln!("Image is too small to decompile");
         }
 
         Ok(())
